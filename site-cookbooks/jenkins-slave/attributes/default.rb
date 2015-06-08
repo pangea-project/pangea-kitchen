8845b59229a54fa0718cf5079715780e57797292
Harald Sitter <sitter@kde.org>
2015-06-08 12:36:07 -0700

    import

---
+++ site-cookbooks/jenkins-slave/attributes/default.rb
@@ -0,0 +1,5 @@
+default.openssh.server.permit_root_login = 'yes'
+default.openssh.server.password_authentication = 'no'
+default.openssh.server.port = '22'
+default.openssh.server.use_p_a_m = 'yes'
+default.openssh.server.print_motd = 'no'
