f795f61e127c2eed73c1843d63a2f65a4f7a46e5
Harald Sitter <sitter@kde.org>
2015-06-09 14:42:28 -0700

    make sure relevant ruby deps are installed

--- bootstrap-ruby.sh
+++ bootstrap-ruby.sh
@@ -4,7 +4,7 @@ set -ex
 
 apt-get update
 apt-get install -y git
-apt-get install -y ruby-dev build-essential wget ssl-cert rsync
+apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev wget ssl-cert rsync
 apt-get purge -y ruby-dev
 
 cd /tmp
