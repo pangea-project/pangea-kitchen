b1da6faf1edceab0c678f35d251089abb3df9cc9
Harald Sitter <sitter@kde.org>
2015-06-12 17:04:43 -0700

    force right user

--- site-cookbooks/jenkins-slave/recipes/default.rb
+++ site-cookbooks/jenkins-slave/recipes/default.rb
@@ -6,6 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 
+slave_home = '/var/lib/jenkins-slave'
+
 include_recipe 'user'
 include_recipe 'openssh'
 
@@ -14,12 +16,27 @@ user_account 'jenkins-slave' do
     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgWtfEVwpqGHHO0JJ3d45wVnobPgexqmRslxbwYj6AuheLxwVdWtZapz+en4+Op9ZS6D70VXW0OmJG7xHaMAq87ZjMcozpp/ez2tUyIpQ3G5Ge7gq/hbhCz+K98pun56ECdhYrQEE/o5jVmG1mfrPDvTGm85PYNrdUVL97PmnOT7aiE58Ljv1EbbSaf/BxjPXrNACZcwmE2WeUJ2jo0wR4KpNIidTfJ/TSy571aX3YO30q8WzuFsTUUt8XQQvKt6r3wGiK9OEGuKjn3OaN6RDxqd/9JvJs700biYx9zmoE8Qmx2cjO5hXREIhEKf1yxtNppXj8A+RAL4+qC7PLzjMV jenkins@rassilon',
     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pDjCY6/xcZ86XYxglQMD9l/wE5neLjkuxXOOp0gumANFhl/X5yiCYQ94qyCnqFoUyhWJUFemTEJ0gBA5q2bjiy/+6yIgVgcDTh93cU+oCDXBuQZOdjGj8H0nKokk3VJxN+z0rM5IlUhJFE/xk4vsWgAag2ZZQtZu+powQLM80jMMTLQSsPjTi29wfsCYQPbBngiqbl/l0EQC1tTEAgWYU3n3Hm0F2nnUn/3wIRe5bN06TEpog+wL9Ap1WB4gak0H4HZ2L1twaPvEhssLDaj/ZlthX4TK0aSN2yKjIkbLr17ZPIyH7GnRXFfvaQmgm9Rr3uWedoYGasV2RVXOIX+P jenkins@river'
   ]
-  home      '/var/lib/jenkins-slave'
+  home slave_home
   ssh_keygen false
+  create_group false
+  uid 100000
+end
+
+group 'jenkins-slave' do
+  action :create
+  append true
+  members %w(jenkins-slave)
+  gid 120
 end
 
 group 'docker' do
   action :modify
   append true
   members %w(jenkins-slave)
+end
+
+execute "chown-yolokitten" do
+  command "chown -R 100000:120 #{slave_home}"
+  user "root"
+  action :run
 end
