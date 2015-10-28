09f05cacb4473aa699cb06d9fb66fbb0402182f5
Rohan Garg <rohan@garg.io>
2015-10-28 12:17:48 +0100

    Disable GPG verification, we use SSH for authentication

--- site-cookbooks/publisher/attributes/default.rb
+++ site-cookbooks/publisher/attributes/default.rb
@@ -1,3 +1,4 @@
 default['aptly']['dist'] = 'nightly'
 default['aptly']['dependencyfollowrecommends'] = true
 default['aptly']['keyserver'] = 'keyserver.ubuntu.com'
+default['aptly']['gpgDisableVerify'] = true
