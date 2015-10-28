90f53e263d902d88169d7d4e05012f89f7267db2
Rohan Garg <rohan@garg.io>
2015-10-28 06:44:41 +0100

    Make sure each user can set ssh keys

--- site-cookbooks/publisher/recipes/default.rb
+++ site-cookbooks/publisher/recipes/default.rb
@@ -13,13 +13,19 @@ package "aptly"
 package "graphviz"
 
 # Requires LWRP'ing so as to enable multiple publishing accounts
+
 repos_setup 'dci' do
   action :setup
   repositories %w(frameworks plasma)
+  sshkeys [
+    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgWtfEVwpqGHHO0JJ3d45wVnobPgexqmRslxbwYj6AuheLxwVdWtZapz+en4+Op9ZS6D70VXW0OmJG7xHaMAq87ZjMcozpp/ez2tUyIpQ3G5Ge7gq/hbhCz+K98pun56ECdhYrQEE/o5jVmG1mfrPDvTGm85PYNrdUVL97PmnOT7aiE58Ljv1EbbSaf/BxjPXrNACZcwmE2WeUJ2jo0wR4KpNIidTfJ/TSy571aX3YO30q8WzuFsTUUt8XQQvKt6r3wGiK9OEGuKjn3OaN6RDxqd/9JvJs700biYx9zmoE8Qmx2cjO5hXREIhEKf1yxtNppXj8A+RAL4+qC7PLzjMV jenkins@rassilon'
+  ]
 end
 
-# Requires LWRP'ing so as to enable multiple publishing accounts
 repos_setup 'kci' do
   action :setup
   repositories %w(frameworks plasma)
+  sshkeys [
+    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pDjCY6/xcZ86XYxglQMD9l/wE5neLjkuxXOOp0gumANFhl/X5yiCYQ94qyCnqFoUyhWJUFemTEJ0gBA5q2bjiy/+6yIgVgcDTh93cU+oCDXBuQZOdjGj8H0nKokk3VJxN+z0rM5IlUhJFE/xk4vsWgAag2ZZQtZu+powQLM80jMMTLQSsPjTi29wfsCYQPbBngiqbl/l0EQC1tTEAgWYU3n3Hm0F2nnUn/3wIRe5bN06TEpog+wL9Ap1WB4gak0H4HZ2L1twaPvEhssLDaj/ZlthX4TK0aSN2yKjIkbLr17ZPIyH7GnRXFfvaQmgm9Rr3uWedoYGasV2RVXOIX+P jenkins@river'
+  ]
 end
--- site-cookbooks/publisher/resources/setup.rb
+++ site-cookbooks/publisher/resources/setup.rb
@@ -1,24 +1,26 @@
 property :uid, String, name_property: true
 property :repositories, Array, default: []
+property :sshkeys, Array, default: []
 
 action :setup do
+  # FIXME: Because I couldn't figure out how to achieve this via
+  #        lazy variables.
+  node.default['aptly']['rootdir'] = "/home/#{uid}"
+
   user_account uid do
-    ssh_keys  [
-      'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgWtfEVwpqGHHO0JJ3d45wVnobPgexqmRslxbwYj6AuheLxwVdWtZapz+en4+Op9ZS6D70VXW0OmJG7xHaMAq87ZjMcozpp/ez2tUyIpQ3G5Ge7gq/hbhCz+K98pun56ECdhYrQEE/o5jVmG1mfrPDvTGm85PYNrdUVL97PmnOT7aiE58Ljv1EbbSaf/BxjPXrNACZcwmE2WeUJ2jo0wR4KpNIidTfJ/TSy571aX3YO30q8WzuFsTUUt8XQQvKt6r3wGiK9OEGuKjn3OaN6RDxqd/9JvJs700biYx9zmoE8Qmx2cjO5hXREIhEKf1yxtNppXj8A+RAL4+qC7PLzjMV jenkins@rassilon',
-      'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pDjCY6/xcZ86XYxglQMD9l/wE5neLjkuxXOOp0gumANFhl/X5yiCYQ94qyCnqFoUyhWJUFemTEJ0gBA5q2bjiy/+6yIgVgcDTh93cU+oCDXBuQZOdjGj8H0nKokk3VJxN+z0rM5IlUhJFE/xk4vsWgAag2ZZQtZu+powQLM80jMMTLQSsPjTi29wfsCYQPbBngiqbl/l0EQC1tTEAgWYU3n3Hm0F2nnUn/3wIRe5bN06TEpog+wL9Ap1WB4gak0H4HZ2L1twaPvEhssLDaj/ZlthX4TK0aSN2yKjIkbLr17ZPIyH7GnRXFfvaQmgm9Rr3uWedoYGasV2RVXOIX+P jenkins@river'
-    ]
     ssh_keygen false
+    ssh_keys sshkeys
     create_group true
+    home node.default['aptly']['rootdir']
   end
 
-  node.default['aptly']['rootdir'] = node['etc']['passwd'][uid]['dir']
+  ohai 'reload' do
+    action :reload
+    plugin 'etc'
+  end
+
   node.default['aptly']['group'] = uid
   node.default['aptly']['user'] = uid
-
-  environment = {
-    'USER' => uid,
-    'HOME' => node.default['aptly']['rootdir']
-  }
 
   template "#{node.default['aptly']['rootdir']}/.aptly.conf" do
     source 'aptly.conf.erb'
@@ -39,6 +41,11 @@ action :setup do
       :ppacodename => node['aptly']['ppacodename']
     })
   end
+
+  environment = {
+    'USER' => uid,
+    'HOME' => node.default['aptly']['rootdir']
+  }
 
   repositories.each do |repo|
     aptly_repo repo do
