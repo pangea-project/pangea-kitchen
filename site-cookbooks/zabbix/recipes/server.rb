3024fc26f260fed55e757baceb80ef8277f5e1af
Harald Sitter <sitter@kde.org>
2015-11-17 10:41:54 +0100

    apply performance modifications for zabbix server
    - mysql connection limit now 512 as zabbix is the only thing using that
      daemon we can go all out here. (up from 100)
    - zabbix pollers set to 16 (up from 5). by default zabbix polls as I found
      out. while we should push in the long run for now simply running more
      pollers reduces the load per poller (busy load was up to >75% with all CI
      hosts being monitored)

--- site-cookbooks/zabbix/recipes/server.rb
+++ site-cookbooks/zabbix/recipes/server.rb
@@ -31,6 +31,46 @@ end
 #   action :create
 # end
 
+config_d = '/etc/zabbix/zabbix_server.conf.d/'
+
+directory config_d do
+  owner 'root'
+  group 'root'
+  mode '0700'
+  action :create
+end
+
+ruby_block 'Twiddle server include' do
+  block do
+    file = Chef::Util::FileEdit.new('/etc/zabbix/zabbix_server.conf')
+    file.search_file_replace_line(/# Include=.*conf\.d.*/,
+                                  "Include=#{config_d}")
+    file.write_file
+  end
+  notifies :restart, 'service[zabbix-server]', :delayed
+end
+
+# This is the amount of pollers zabbix starts. We want this to be roughly
+# equal to the amount of hosts we montior. Once we run out of RAM, push
+# notification needs to be investigated.
+file "#{config_d}/StartPollers.conf" do
+  content 'StartPollers=16'
+  mode '0600'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[zabbix-server]', :delayed
+end
+
+# Increase connection limit. Servers are meant to exclusively run zabbix, so
+# we can give it a reasonable amount of connections.
+file '/etc/mysql/conf.d/max_connections' do
+  content 'max_connections = 512'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[mysql]', :delayed
+end
+
 ruby_block 'Twiddle timezone' do
   block do
     file = Chef::Util::FileEdit.new('/etc/apache2/conf-available/zabbix.conf')
