f4184f0324e59e2bc2e8a815b014ab8cdcb65d0f
Harald Sitter <sitter@kde.org>
2015-12-18 11:39:22 +0100

    sort!

--- site-cookbooks/publisher/metadata.rb
+++ site-cookbooks/publisher/metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures repos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
 depends 'aptly'
 depends 'user'
-depends 'apt'
