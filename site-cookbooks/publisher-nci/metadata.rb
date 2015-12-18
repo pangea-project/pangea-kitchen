4d3e930149470e22cec4e4d231a64d128fdd6c0b
Harald Sitter <sitter@kde.org>
2015-12-18 11:39:52 +0100

    let nci's publisher setup gpg and apache

--- site-cookbooks/publisher-nci/metadata.rb
+++ site-cookbooks/publisher-nci/metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures publisher-nci'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apache2', '~> 3.1.0'
+depends 'apt'
+depends 'aptly'
+depends 'bsw_gpg', '~> 0.2.3'
+depends 'publisher'
+depends 'user'
--- site-cookbooks/publisher-nci/recipes/default.rb
+++ site-cookbooks/publisher-nci/recipes/default.rb
@@ -7,15 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#
-# Cookbook Name:: scrapper-ci
-# Recipe:: default
-#
-# Copyright 2015, Harald Sitter <sitter@kde.org>
-#
-# All rights reserved - Do Not Redistribute
-#
-
 ## FIXME: codecopy
 include_recipe 'apt'
 
@@ -32,10 +23,29 @@ package 'aptly'
 package 'graphviz'
 ## FIXME: codecopy
 
+# Publisher User
 publisher_setup 'nci' do
   action :setup
   sshkeys [
     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pDjCY6/xcZ86XYxglQMD9l/wE5neLjkuxXOOp0gumANFhl/X5yiCYQ94qyCnqFoUyhWJUFemTEJ0gBA5q2bjiy/+6yIgVgcDTh93cU+oCDXBuQZOdjGj8H0nKokk3VJxN+z0rM5IlUhJFE/xk4vsWgAag2ZZQtZu+powQLM80jMMTLQSsPjTi29wfsCYQPbBngiqbl/l0EQC1tTEAgWYU3n3Hm0F2nnUn/3wIRe5bN06TEpog+wL9Ap1WB4gak0H4HZ2L1twaPvEhssLDaj/ZlthX4TK0aSN2yKjIkbLr17ZPIyH7GnRXFfvaQmgm9Rr3uWedoYGasV2RVXOIX+P jenkins@river'
   ]
   apiport 9090
+end
+
+# GPG
+include_recipe 'bsw_gpg::default'
+bsw_gpg_load_key_from_string 'a string key' do
+  key_contents KeyBag.load("#{uid}.private.key")
+  for_user uid
+end
+
+# Apache
+web_app 'nci_web_repo' do
+  # server_name node['hostname']
+  # server_aliases [node['fqdn'], 'neon.pangea.pub']
+  server_name 'localhost'
+  server_aliases 'localhost'
+  server_port 9054
+  docroot '/home/nci/aptly/public'
+  cookbook 'apache2'
 end
