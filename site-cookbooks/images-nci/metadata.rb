c5230aaee2ab8fdd3c6ae88a317525ca3b578830
Jonathan Riddell <jr@jriddell.org>
2016-02-08 14:11:00 +0000

    images-nci depends on apache

--- site-cookbooks/images-nci/metadata.rb
+++ site-cookbooks/images-nci/metadata.rb
@@ -5,3 +5,5 @@ license          'GNU Public License 2.0'
 description      'Installs/Configures images-nci2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apache2'
