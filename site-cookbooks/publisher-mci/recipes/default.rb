ab6adc29d804edacc3e540cf6397e60b50b4cb0c
Harald Sitter <sitter@kde.org>
2016-02-19 11:58:25 +0100

    don't setup ssh keys for publishers, we don't use them currently that way

--- site-cookbooks/publisher-mci/recipes/default.rb
+++ site-cookbooks/publisher-mci/recipes/default.rb
@@ -26,9 +26,7 @@ package 'graphviz'
 # Publisher User
 publisher_setup 'mci' do
   action :setup
-  sshkeys [
-    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pDjCY6/xcZ86XYxglQMD9l/wE5neLjkuxXOOp0gumANFhl/X5yiCYQ94qyCnqFoUyhWJUFemTEJ0gBA5q2bjiy/+6yIgVgcDTh93cU+oCDXBuQZOdjGj8H0nKokk3VJxN+z0rM5IlUhJFE/xk4vsWgAag2ZZQtZu+powQLM80jMMTLQSsPjTi29wfsCYQPbBngiqbl/l0EQC1tTEAgWYU3n3Hm0F2nnUn/3wIRe5bN06TEpog+wL9Ap1WB4gak0H4HZ2L1twaPvEhssLDaj/ZlthX4TK0aSN2yKjIkbLr17ZPIyH7GnRXFfvaQmgm9Rr3uWedoYGasV2RVXOIX+P jenkins@river'
-  ]
+  sshkeys []
   apiport 9090
 end
 
