737705e5b98df92f6dec5688ed87f2c9cf99cab7
Harald Sitter <sitter@kde.org>
2016-06-30 16:36:53 +0200

    attempt to support xenial

--- site-cookbooks/jenkins-java-trusty/recipes/default.rb
+++ site-cookbooks/jenkins-java-trusty/recipes/default.rb
@@ -6,10 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 
-package 'openjdk-6-jre-headless openjdk-6-jre-lib' do
-  action :purge  
+to_purge = %w(openjdk-6-jre-headless openjdk-6-jre-lib)
+to_install = %w(default-jre-headless openjdk-8-jre-headless)
+
+if Chef::VersionConstraint.new('= 16.04').include?(node['platform_version'])
+  to_purge = []
+  to_install = %w(default-jre-headless openjdk-8-jre-headless)
 end
 
-package 'default-jre-headless openjdk-7-jre-headless' do
+package 'jenkins-java purge' do
+  package_name to_purge
+  action :purge
+end
+
+package 'jenkins-java install' do
+  package_name to_install
   action :install
 end
