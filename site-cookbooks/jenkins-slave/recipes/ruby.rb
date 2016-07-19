90f4f46f9c48c11616046ffe565c722e674b3324
Harald Sitter <sitter@kde.org>
2016-07-19 17:12:34 +0200

    add new dep for gir

--- site-cookbooks/jenkins-slave/recipes/ruby.rb
+++ site-cookbooks/jenkins-slave/recipes/ruby.rb
@@ -9,7 +9,8 @@
 include_recipe 'ruby_build::default'
 
 # additional build depends
-package %w(libssl-dev libreadline-dev zlib1g-dev build-essential)
+package %w(libssl-dev libreadline-dev zlib1g-dev build-essential
+           libgirepository1.0-dev libglib2.0-dev)
 
 # remove any system rubies (bound to 14.04)
 apt_package 'ruby' do
