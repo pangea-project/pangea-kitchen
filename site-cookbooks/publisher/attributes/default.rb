419e312836ab03fe255e5c25b3e8fa1c57369fbb
Rohan Garg <rohan@garg.io>
2016-08-03 14:59:54 +0200

    Update recipe for publisher

--- site-cookbooks/publisher/attributes/default.rb
+++ site-cookbooks/publisher/attributes/default.rb
@@ -1,5 +1,5 @@
-default['aptly']['dist'] = 'nightly'
 default['aptly']['dependencyfollowrecommends'] = true
 default['aptly']['keyserver'] = 'keyserver.ubuntu.com'
 default['aptly']['gpgDisableVerify'] = true
 default['aptly']['serve_security_hole'] = false
+default['aptly']['architectures'] = %w(amd64 armhf i386 all source)
