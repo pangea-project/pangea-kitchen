8a0a3848296be5ac8dfd7540972cc045c1d42e51
Harald Sitter <sitter@kde.org>
2016-11-30 17:13:11 +0100

    also loosen apach2 lock in site cookbooks

--- site-cookbooks/publisher-mci/metadata.rb
+++ site-cookbooks/publisher-mci/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures publisher-mci'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apache2', '~> 3.1.0'
+depends 'apache2', '~> 3.1'
 depends 'apt'
 depends 'aptly'
 depends 'bsw_gpg', '~> 0.2.3'
