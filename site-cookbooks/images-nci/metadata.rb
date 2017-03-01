bc62df0e84e4dd79151f07ba49e65a22b9d8289d
Jonathan Riddell <jr@jriddell.org>
2017-03-01 17:17:10 +0000

    remove images-nci, now handled by kde sysadmin on other machines

--- site-cookbooks/images-nci/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'images-nci'
-maintainer       'Jonathan Riddell'
-maintainer_email 'jr@jriddell.org'
-license          'GNU Public License 2.0'
-description      'Installs/Configures images-nci2'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'apache2'
