dba1ecbe342de86d657aad61216ffaf7066e6a66
Harald Sitter <sitter@kde.org>
2017-03-02 15:36:51 +0100

    add submodule for cupboard

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,18 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+
+def submod
+  `git config --local --get include.path`
+  unless $?.success?
+    system('git submodule init') || raise
+    system('git submodule update --remote') || raise
+    system('git config --local include.path ../.gitconfig') || raise
+    system('git fetch --verbose') || raise
+  end
+  system('git submodule update --remote --recursive') || raise
+  cupboard_dir = "#{__dir__}/../data_bags/cupboard"
+  puts 'Calling git secret reveal!'
+  system('./bin/git-secret reveal', chdir: cupboard_dir) || raise
+end
+submod
