6f84714e16021b16753cef0dd3da08f5d063e3af
Jonathan Riddell <jr@jriddell.org>
2017-03-02 17:24:29 +0000

    add jobs directory and link

--- site-cookbooks/jenkins-master/recipes/default.rb
+++ site-cookbooks/jenkins-master/recipes/default.rb
@@ -117,3 +117,16 @@ mount '/mnt/volume-neon-jenkins' do
   device '/dev/sda1'
   fstype 'ext4'
 end
+
+directory '/mnt/volume-neon-jenkins/jobs/' do
+  owner 'jenkins'
+  group 'jenkins'
+  mode '0755'
+  action :create
+end
+
+link '/var/lib/jenkins/jobs' do
+  owner 'jenkins'
+  group 'jenkins'
+  to '/mnt/volume-neon-jenkins/jobs/'
+end
