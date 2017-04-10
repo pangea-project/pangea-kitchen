2d2ce529903aed7de66801202b16614ecb107079
Harald Sitter <sitter@kde.org>
2017-04-10 12:09:47 +0200

    request production cert

--- site-cookbooks/certbot/resources/apache.rb
+++ site-cookbooks/certbot/resources/apache.rb
@@ -3,7 +3,7 @@ property :email, String
 
 action :create do
   execute "certbot-apache-#{domains.join('_')}" do
-    command 'certbot --apache --test-cert --non-interactive --agree-tos' \
+    command 'certbot --apache --non-interactive --agree-tos' \
             " --no-eff-email --redirect --email #{email}" \
             " #{domains.map { |k| "-d #{k}" }.join(' ')}"
   end
