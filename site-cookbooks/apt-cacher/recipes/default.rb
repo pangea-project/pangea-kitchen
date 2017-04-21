5d3ae306bda7a4e7c77fed8e861081debfddfa3a
Harald Sitter <sitter@kde.org>
2017-04-21 18:03:27 +0200

    migrate apt-cacher from apt-cacher-ng to squid-deb-proxy

--- site-cookbooks/apt-cacher/recipes/default.rb
+++ site-cookbooks/apt-cacher/recipes/default.rb
@@ -18,25 +18,66 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-include_recipe 'apt::cacher-ng'
+package 'squid-deb-proxy'
 
-data_bag_path = Chef::Config[:data_bag_path]
-security_conf = "#{data_bag_path}/cupboard/apt-cacher-ng/security.conf"
-template '/etc/apt-cacher-ng/security.conf' do
-  source security_conf
-  local true
-  owner 'root'
-  group 'apt-cacher-ng'
-  mode 0o640
-  notifies :restart, 'service[apt-cacher-ng]'
+apt_package %w[apt-cacher-ng avahi-daemon] do
+  # apt-acher-ng was used previously but turned out a bit shitty, so we force
+  # a migration to squid instead.
+  #
+  # squid-deb-proxy registers an avahi service and we can't do anything about
+  # that but remove avahi entirely. Not useful anyway.
+  # NOTE: the service also means stopping squid shits its pants, so we need
+  #   to disable it so get the stupid squid-deb-proxy service to properly stop
+  action :purge
 end
 
-template '/etc/apt-cacher-ng/optimize.conf' do
-  source 'optimize.conf'
-  owner 'root'
-  group 'apt-cacher-ng'
-  mode 0o640
-  notifies :restart, 'service[apt-cacher-ng]'
+file '/etc/squid-deb-proxy/mirror-dstdomain.acl.d/0-neon' do
+  # NB: the concatenation helper of init-common is shitty and needs a \n to
+  #    not break the concatenated file
+  content ".archive.neon.kde.org\n"
+end
+
+ruby_block 'twiddle squid-deb-proxy.conf' do
+  block do
+    file = Chef::Util::FileEdit.new('/etc/squid-deb-proxy/squid-deb-proxy.conf')
+    file.search_file_replace_line(/http_access deny !to_archive_mirrors/,
+                                  'http_access allow to_archive_mirrors')
+    file.insert_line_if_no_match(/http_access allow localhost manager/)
+    file.search_file_replace_line(/## http_access deny !to_archive_mirrors/,
+                                  'http_access allow to_archive_mirrors')
+                                  http_access allow to_archive_mirrors
+    file.write_file
+  end
+end
+
+# This is very specific custom workaround bullshit.
+# squid-deb-proxy uses upstart and doesn't actually properly shut down squid!
+# To get working shutdown etc we use a custom systemd service. This is
+# using part of the deb-proxy init-common rigging to concat directory acls
+# and manages squid properly via -k.
+# Should the init-common stuff change substantially this may either set up
+# incomplete or fail entirely. Not much to be done alas.
+systemd_unit 'squid-deb-proxy.service' do
+  content <<-EOF
+[Unit]
+Description=squid-deb-proxy
+
+[Service]
+ExecStartPre=/bin/bash -c '. /usr/share/squid-deb-proxy/init-common.sh; pre_start'
+ExecStart=/usr/sbin/squid -N -f /etc/squid-deb-proxy/squid-deb-proxy.conf
+ExecStartPost=/bin/bash -c '. /usr/share/squid-deb-proxy/init-common.sh; post_start'
+ExecStop=/usr/sbin/squid -k shutdown -f /etc/squid-deb-proxy/squid-deb-proxy.conf
+ExecReload=/usr/sbin/squid -k reconfigure -f /etc/squid-deb-proxy/squid-deb-proxy.conf
+
+[Install]
+WantedBy=multi-user.target
+  EOF
+  action %i[create enable start]
+end
+
+systemd_unit 'squid.service' do
+  # Disable regular squid, we don't need it
+  action %i[disable stop]
 end
 
 # Wire into apache.
@@ -60,7 +101,8 @@ template "#{node['apache']['dir']}/sites-available/#{server_name}.conf" do
   owner 'root'
   group node['apache']['root_group']
   mode 0o644
-  variables server_name: server_name, document_root: document_root
+  variables server_name: server_name, document_root: document_root,
+            proxy_port: 8000
   # Reload apache immediately so the vhost is up and running by the time
   # certbot does its thing.
   notifies :reload, 'service[apache2]', :immediately
@@ -72,5 +114,6 @@ end
 
 certbot_apache server_name do
   domains [server_name]
+  redirect false
   email 'sitter@kde.org'
 end
--- site-cookbooks/certbot/resources/apache.rb
+++ site-cookbooks/certbot/resources/apache.rb
@@ -1,10 +1,12 @@
 property :domains, Array
 property :email, String
+property :redirect, kind_of: [TrueClass, FalseClass], default: true
 
 action :create do
   execute "certbot-apache-#{domains.join('_')}" do
     command 'certbot --apache --non-interactive --agree-tos' \
-            " --no-eff-email --redirect --email #{email}" \
+            " --no-eff-email --email #{email}" \
+            " #{redirect ? '--redirect' : ''}" \
             " #{domains.map { |k| "-d #{k}" }.join(' ')}"
   end
 end
