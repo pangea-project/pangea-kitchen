581d8eef9f10998452ce3ddc9ee2b9ce19af8e29
Harald Sitter <sitter@kde.org>
2017-05-03 16:35:03 +0200

    allow certbot to dryrun in vagrant

--- site-cookbooks/certbot/resources/apache.rb
+++ site-cookbooks/certbot/resources/apache.rb
@@ -4,9 +4,10 @@ property :redirect, kind_of: [TrueClass, FalseClass], default: true
 
 action :create do
   execute "certbot-apache-#{domains.join('_')}" do
-    command 'certbot --apache --non-interactive --agree-tos' \
-            " --no-eff-email --email #{email}" \
-            " #{redirect ? '--redirect' : ''}" \
-            " #{domains.map { |k| "-d #{k}" }.join(' ')}"
+    args = %w[--apache --non-interactive --agree-tos --no-eff-email]
+    args << '--redirect' if redirect
+    args << '--dyr-run' if node.name.include?('vagrant')
+    args << domains.map { |k| "-d #{k}" }.join(' ')
+    command "certbot #{args.join(' ')}"
   end
 end
