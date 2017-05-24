da2b4fe5c5c7211d600dc2374da374b55e614c2d
Harald Sitter <sitter@kde.org>
2017-05-24 10:23:50 +0200

    declare that apt-cacher effecitvely needs chef13 now

--- site-cookbooks/apt-cacher/metadata.rb
+++ site-cookbooks/apt-cacher/metadata.rb
@@ -5,6 +5,8 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures apt-cacher'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+chef_version '>= 13'
 depends 'apache2'
 depends 'apt'
 depends 'certbot'
