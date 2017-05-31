c1f6f449d0785939003530b5f1788f0638ec94ae
Harald Sitter <sitter@kde.org>
2017-05-31 13:26:20 +0200

    allow localhost as query server (for server itself we get traffic over lo)

--- site-cookbooks/zabbix/attributes/default.rb
+++ site-cookbooks/zabbix/attributes/default.rb
@@ -1,2 +1,2 @@
 default['zabbix']['server']['node'] = 'omega'
-default['zabbix']['server']['ipaddress'] = 'monitoring.pangea.pub'
+default['zabbix']['server']['ipaddress'] = 'monitoring.pangea.pub,127.0.0.1'
