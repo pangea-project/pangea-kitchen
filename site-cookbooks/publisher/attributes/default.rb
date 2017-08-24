47e950668aadfabd7745b8899dd8f33647a5a52f
Rohan Garg <rohan@garg.io>
2017-08-24 16:44:16 +0200

    Make sure we also configure arm64

--- site-cookbooks/publisher/attributes/default.rb
+++ site-cookbooks/publisher/attributes/default.rb
@@ -2,4 +2,4 @@ default['aptly']['dependencyfollowrecommends'] = true
 default['aptly']['keyserver'] = 'keyserver.ubuntu.com'
 default['aptly']['gpgDisableVerify'] = true
 default['aptly']['serve_security_hole'] = false
-default['aptly']['architectures'] = %w(amd64 armhf i386 all source)
+default['aptly']['architectures'] = %w(amd64 armhf i386 all source arm64)
