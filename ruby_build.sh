627b9c4b3e35c66cd73a14e728c2e0aebf8e6af7
Harald Sitter <sitter@kde.org>
2017-09-21 12:10:07 +0200

    exit 1 on new ruby version, we expect the caller to handle this

--- ruby_build.sh
+++ ruby_build.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+#
+# Copyright (C) 2017 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# Exits:
+# 0:    no change
+# 1:    change
+# else: error
 
 set -ex
 
@@ -16,3 +39,9 @@ export NO_CUPBOARD=1 # Disable cupboard use (requires manual unlocking)
 knife supermarket install ruby_build
 
 chef-client --local-mode --enable-reporting  -o 'pangea-ruby::install'
+
+exit 1
+# The rationale behind exit 1 is that installing a new ruby means that part of
+# the calling utils need to reload in order to actually run it. If they do
+# not explicitly handle the exit status codes exit 1 will at least cause
+# a teardown, next time the utils are run the version should be aligned then.
