ee23b807bf76bd75a5286c38b9d458106c6d9385
Scarlett Clark <sgclark@kde.org>
2018-01-30 21:04:16 +0100

    ruby_build knife command is failing on brand new containers. Add -V temproarily to try and debug.

--- ruby_build.sh
+++ ruby_build.sh
@@ -44,7 +44,7 @@ fi
 export NO_CUPBOARD=1 # Disable cupboard use (requires manual unlocking)
 
 # Instead of berksing this, use knife to download the single dependency. Faster.
-knife supermarket install ruby_build
+knife -V supermarket install ruby_build
 
 chef-client --local-mode --enable-reporting  -o 'pangea-ruby::install'
 
