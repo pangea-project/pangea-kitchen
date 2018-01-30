ed7ea0940f349d21400c7585826cf6f13a02108e
Scarlett Clark <sgclark@kde.org>
2018-01-30 21:14:56 +0100

    Correct placement of -V

--- ruby_build.sh
+++ ruby_build.sh
@@ -44,7 +44,7 @@ fi
 export NO_CUPBOARD=1 # Disable cupboard use (requires manual unlocking)
 
 # Instead of berksing this, use knife to download the single dependency. Faster.
-knife -V supermarket install ruby_build
+knife supermarket install ruby_build -VV
 
 chef-client --local-mode --enable-reporting  -o 'pangea-ruby::install'
 
