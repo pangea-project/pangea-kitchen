713be56f1b4d8d75d4502d717aaf0a54a2a55835
Scarlett Clark <sgclark@kde.org>
2018-01-31 19:07:35 +0100

    Revert "ruby_build.sh does not use Berkshelf so changing compat_resource there has no affect. Try to install"
    This reverts commit 5f131714576ff2774db37235de08d87289cd0b16.

--- ruby_build.sh
+++ ruby_build.sh
@@ -44,7 +44,6 @@ fi
 export NO_CUPBOARD=1 # Disable cupboard use (requires manual unlocking)
 
 # Instead of berksing this, use knife to download the single dependency. Faster.
-knife supermarket install compat_resource -VV
 knife supermarket install ruby_build -VV
 
 chef-client --local-mode --enable-reporting  -o 'pangea-ruby::install'
