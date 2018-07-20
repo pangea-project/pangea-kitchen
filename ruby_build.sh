690e83d17feb773c763cd4ae83aff60e18be9838
Harald Sitter <sitter@kde.org>
2018-07-20 15:13:51 +0200

    Revert " knife install is currently broken: https://github.com/blue-systems/pangea-kitchen/issues/3"
    the commit breaks bootstrap AND is no longer necessary as per the issue
    it references
    
    This reverts commit f55712d5bbe14de15ebfe823532a0fcfc1fbdcd3.
    
    # Conflicts:
    #	ruby_build.sh

--- ruby_build.sh
+++ ruby_build.sh
@@ -44,12 +44,7 @@ fi
 export NO_CUPBOARD=1 # Disable cupboard use (requires manual unlocking)
 
 # Instead of berksing this, use knife to download the single dependency. Faster.
-# knife supermarket install ruby_build -VV
-# knife install is currently broken: https://github.com/pangea-project/pangea-kitchen/issues/3
-# berks however works, use that for now until above is resolved.
-bundle install
-berks install # lock cookbook dependencies
-berks vendor  # install cookbook dependencies in berks-cookbooks
+knife supermarket install ruby_build -VV
 
 chef-client --local-mode --enable-reporting  -o 'pangea-ruby::install'
 
