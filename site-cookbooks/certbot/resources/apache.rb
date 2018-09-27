d8461f7624307ba72b38c6edc49606b3fc7121b0
Harald Sitter <sitter@kde.org>
2018-09-27 12:57:51 +0200

    deprecation--
    https://docs.chef.io/deprecations_namespace_collisions.html

--- site-cookbooks/certbot/resources/apache.rb
+++ site-cookbooks/certbot/resources/apache.rb
@@ -6,12 +6,12 @@ action :create do
   include_recipe 'apache2::default'
   include_recipe 'apache2::mod_ssl'
   include_recipe 'certbot::default'
-  execute "certbot-apache-#{domains.join('_')}" do
+  execute "certbot-apache-#{new_resource.domains.join('_')}" do
     args = %w[--apache --non-interactive --agree-tos --no-eff-email]
-    args << '--email' << email
-    args << '--redirect' if redirect
+    args << '--email' << new_resource.email
+    args << '--redirect' if new_resource.redirect
     args << '--dyr-run' if node.name.include?('vagrant')
-    args << domains.map { |k| "-d #{k}" }.join(' ')
+    args << new_resource.domains.map { |k| "-d #{k}" }.join(' ')
     command "certbot #{args.join(' ')}"
   end
 end
