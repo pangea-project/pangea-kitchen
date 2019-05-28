d0af6496c040e3782c08f7fa4ba188617e328fe3
Harald Sitter <sitter@kde.org>
2019-05-28 09:17:57 +0200

    openssh cookbook has native support for revoked_keys -> use it

--- site-cookbooks/server-common/attributes/default.rb
+++ site-cookbooks/server-common/attributes/default.rb
@@ -23,7 +23,10 @@ default[:openssh][:server][:x11_forwarding] = 'no'
 default[:openssh][:server][:ignore_rhosts] = 'yes'
 # List of keys to prevent from getting used *at all*. This cannot be overridden
 # the keys are out right rejected. Used for former company admins.
-default[:openssh][:server][:revoked_keys] = '/etc/ssh/revoked_keys'
+default[:openssh][:server][:revoked_keys] = [
+  ## Rohan Garg <garg@kde.org>
+  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDAZWkRp7cRdMELEt+7OXwQVqvh9zB5uMLSPqHSnrYoHBgQkiZZfihfFX8mt/aSQuZVRmj3JfcmVLq666jX20kJ8CxyfQ4gs3OXgNcALZaHKufPz1nRewqB/r27xa46rXA9lSnAywkZN3KxvZGWy9jhzcCnakqesIUuWgnYwzOL5PjeIWpbIVAbz9eVc29wSn6QRiGQswJqpTiI8zCyvrv2M0tc2Ntnl6SVf4PeYcUkwW2pvtUatv3bRNfLYgUQ8E6zuUm4Tv2gNhev4UYcRaIWres/g3Ie5GQUh/T+YN5pnkL7e0mOviUQ57IH9LoCkmdAQg+xkXH4TKmaT/cTa85WS3O9ys6jmRoiaoKVyfFh0mrLWnxjZj4ubYGIajqB0cGaiqoqRciN6Qslf00Rm8CuHHzlNgh3oVB7hbFU/+ztWqYkIr0cyyozHKTF/j4jAapMI8RLk3Jy3HRi+q+epGwZjGsnVRNHiZYKDAcJhlBr6SsRJz/7uxjmGO8Wh9zjAdV/MdE2Z8xamhHgNrYlqfUT4TZl2bIb6JCrXy+Z7Pga9k+8ergHkF348Uy+ngluELG+DXKRjwsAWzRbfWc6OIM5g2fKP+NbiyGVu6lzoe6T9e6HIVrnbO4S/7Fpat4gkz3xC/rO4H3e/i7In2h4g+j+rAAeZsj0R7poIQKb6p1QPw== shadeslayer@saphira'
+]
 
 # Fail2Ban
 default[:fail2ban][:services][:ssh][:maxretry] = '2'
