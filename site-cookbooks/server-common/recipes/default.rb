96e17a214816c27810c30709a967d1b404bb7db4
Harald Sitter <sitter@kde.org>
2019-05-28 08:58:38 +0200

    clean up motd on all servers
    remove all advertisement crap and also the standard help text and sysinfo.
    not once has any of this ever been useful. this generally leaves only
    the pending-updates info + uname + lastlogin active and the entire
    motd much cleaner

--- site-cookbooks/server-common/recipes/default.rb
+++ site-cookbooks/server-common/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: server-common
 # Recipe:: default
 #
-# Copyright 2016-2018, Harald Sitter <sitter@kde.org>
+# Copyright 2016-2019, Harald Sitter <sitter@kde.org>
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -27,6 +27,31 @@ systemd_journald 'enable-peristence-without-syslog' do
   notifies :restart, 'service[systemd-journald-enable-peristence]', :delayed
   only_if do
     [node.fetch('platform'), node.fetch('platform_version')] != %w(ubuntu 14.04)
+  end
+end
+
+# Disable canonical news nonesense. Nobody gives a flying F.
+package 'ubuntu-advantage-tools' do
+  action :purge
+end
+
+file '/etc/update-motd.d/10-help-text' do
+  action :delete
+end
+
+file '/etc/update-motd.d/50-landscape-sysinfo' do
+  action :delete
+end
+
+file '/etc/update-motd.d/51-cloudguest' do
+  action :delete
+end
+
+ruby_block 'disable-canonical-news' do
+  block do
+    file = Chef::Util::FileEdit.new('/etc/default/motd-news')
+    file.search_file_replace_line('ENABLED=0', 'ENABLED=1')
+    file.write_file
   end
 end
 
