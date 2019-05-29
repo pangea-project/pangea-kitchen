833ca0ad7ebbc18986d2af7e830ced2eb2550584
Harald Sitter <sitter@kde.org>
2019-05-29 11:17:38 +0200

    add certbot support to jenkins-master-apache cookbook
    this somehow was implied in some nodes but doesn't actually exist as a
    feature strangely enough

--- site-cookbooks/jenkins-master-apache/recipes/default.rb
+++ site-cookbooks/jenkins-master-apache/recipes/default.rb
@@ -16,7 +16,8 @@ params = {
   server_name: node['jenkins-master-apache']['server_name'],
   server_aliases: node['jenkins-master-apache']['server_aliases'],
   jenkinsroot: '/var/cache/jenkins',
-  jenkinshome: '/var/lib/jenkins'
+  jenkinshome: '/var/lib/jenkins',
+  document_root: "/var/www/#{node['jenkins-master-apache']['server_name']}"
 }
 
 application_name = params[:server_name]
@@ -26,7 +27,10 @@ include_recipe 'apache2::default'
 include_recipe 'apache2::mod_proxy'
 include_recipe 'apache2::mod_proxy_http'
 
-service_reload_type = :delayed
+directory params[:document_root] do
+  owner 'www-data'
+  group 'www-data'
+end
 
 template "#{node['apache']['dir']}/sites-available/#{application_name}.conf" do
   source params[:template]
@@ -39,13 +43,20 @@ template "#{node['apache']['dir']}/sites-available/#{application_name}.conf" do
     application_name: application_name,
     params: params
   )
-  notifies :reload, 'service[apache2]', service_reload_type
+  notifies :reload, 'service[apache2]', :delayed
 end
 
 site_enabled = params[:enable]
 apache_site params[:name] do
   enable site_enabled
-  notifies :reload, 'service[apache2]', service_reload_type
+  notifies :reload, 'service[apache2]', :immediately # needs to be for certbot
+end
+
+certbot_apache params[:server_name] do
+  domains [params[:server_name]]
+  email 'sitter@kde.org'
+  webroot_path params[:document_root]
+  only_if { site_enabled && node['jenkins-master-apache']['certbot'] }
 end
 
 # Say thanks to Riddell for this.
