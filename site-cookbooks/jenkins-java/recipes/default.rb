e353136f8a741055c17df1c5a94f8e9260bcb744
Harald Sitter <sitter@kde.org>
2020-11-26 13:50:12 +0100

    only install jre8 on focal as well
    I believe we could use 11 but since I'm not sure it's probably safer to
    say with 8

--- site-cookbooks/jenkins-java/recipes/default.rb
+++ site-cookbooks/jenkins-java/recipes/default.rb
@@ -7,13 +7,16 @@
 # All rights reserved - Do Not Redistribute
 
 to_purge = []
-to_install = %w[default-jre-headless openjdk-8-jre-headless]
+to_install = []
 
-if Chef::VersionConstraint.new('= 18.04').include?(node['platform_version'])
+if Chef::VersionConstraint.new('= 18.04').include?(node['platform_version']) ||
+   Chef::VersionConstraint.new('= 20.04').include?(node['platform_version'])
   # Default on 18.04 is openjdk11, but jenkins LTS is wants openjdk8 or it
   # won't even start.
   to_purge = %w[default-jre-headless openjdk-11-jre-headless]
   to_install = %w[openjdk-8-jre-headless]
+else
+  to_install = %w[default-jre-headless]
 end
 
 package 'jenkins-java purge' do
