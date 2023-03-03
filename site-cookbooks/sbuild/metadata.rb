a35d12e7c787dea8db0ca48411b5ffce153702c7
Harald Sitter <sitter@kde.org>
2023-03-03 14:24:03 +0100

    throw away unused crap

--- site-cookbooks/sbuild/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'sbuild'
-maintainer       'Rohan Garg'
-maintainer_email 'rohan@garg.io'
-license          'All rights reserved'
-description      'Installs/Configures sbuild'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- site-cookbooks/zabbix/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['zabbix']['server']['node'] = 'omega'
-default['zabbix']['server']['ipaddress'] = 'monitoring.pangea.pub,127.0.0.1'
